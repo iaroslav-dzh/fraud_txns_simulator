6e6d7ac706cc22565b9ad24493650118ec59d581
iaroslav-dzh <mutuandrea94@gmail.com>
2025-07-14 14:28:34 +0300

    DropConfigBuilder moved to builder.py

--- modules/data_generator/drafts/drfts_drops.py
+++ modules/data_generator/drafts/drfts_drops.py
@@ -70,53 +70,6 @@ class DropLifecycleManager:
         self.reset_cache() # сброс всего кэша после завершения активности дропа
 
 
-# Возможный метод для DistBehavior
-@property
-def to_crypto(self):
-    """
-    """
-    # Если не онлайн, то невозможен перевод в крипту
-    if not self.online: 
-        return
-    
-    drop_rate = self.to_drop_rate
-    # Возвращаем True или False
-    return np.random.uniform(0,1) < drop_rate
-
-
-def switch_online(self, declined):
-    """
-    """
-    if not declined:
-        return
-    scen = self.scen
-    if scen in ["atm", "atm+transfer"]:
-        pass
-
-def switch_online(self, prev_declined):
-    """
-    Направляет выполнение сценария.
-    Записывает True или False в self.online с точки зрения какая 
-    должна быть транзакция: онлайн или оффлайн (перевод или снятие).
-    ------------
-    """
-    if not prev_declined:
-        return
-    
-    scen = self.scen
-    {"method01->method02":0.6,
-     "method01->method01":0.4}
-    # В atm+transfer только первая транзакция может быть atm(оффлайн)
-    if scen == "atm+transfer" and not self.last_online:
-        self.online = True
-    elif scen == "atm+transfer"and self.last_online:
-        self.online = False
-    elif scen == "atm":
-        self.online = True
-    elif scen in ["split_transfer", "transfer"]:
-        self.online = True
-
-
 # import data_generator.fraud.drops.base
 # import data_generator.indev
 # import data_generator.configs
@@ -168,118 +121,13 @@ config3 = drop_cfg_build.build_dist_cfg()
 drop_amt1 = DropAmountHandler(configs=config3)
 dist_behav1 = DistBehaviorHandler(configs=config3, amt_hand=drop_amt1)
 
+cfg_builder = CfgBuild(base_cfg, fraud_cfg, drops_cfg)
+configs = cfg_builder.build_cfg() # метод в зависимости от типа дропа
+acc_hand = AccHand(configs)
+amt_hand = AmtHand(configs)
+time_hand = TimeHand(configs)
+behav_hand = BehavHand(configs, amt_hand) # класс в зависимости от типа дропа
+part_data = PartData(configs)
+base_classes = BaseCls(acc_hand, amt_hand, time_hand, behav_hand, part_data) # индивидуальный behav_hand
 
-# `split_rate` = 0.7  
-# Тест `large_balance`: баланс больше чем amt_max
-
-# Вероятности сценариев для large_balance
-split_rate = 0.7
-p_atm_and_trf = split_rate * 0.5
-p_split_trf = split_rate * 0.5
-p_atm = 1 - split_rate
-print(f"""atm+transfer prob: {p_atm_and_trf}
-split_transfer prob: {p_split_trf}
-atm prob: {p_atm:.3f}""")
-assert sum([p_atm_and_trf, p_split_trf, p_atm]) == 1
-
-all_scens1 = []
-i = 0
-while i < 2000:
-    drop_amt1.reset_cache()
-    drop_amt1.balance = dist_behav1.trf_max + 1000
-    assert drop_amt1.balance > dist_behav1.trf_max, "Balance is below trf_max"
-    dist_behav1.sample_scenario()
-    # print(dist_behav1.scen)
-    all_scens1.append(dist_behav1.scen)
-    i += 1
-all_scens_ser1 = pd.Series(all_scens1)
-
-all_scens_ser1.value_counts(normalize=True)
-
-
-# `split_rate` = 0.7  
-# Тест `split_eligible`: баланс больше чем `self.amt_min * 2`,  
-# но меньше чем `self.amt_max`
-
-# Вероятности сценариев для split_eligible
-split_rate = 0.7
-p_split_trf = split_rate
-p_trf = 1 - split_rate
-print(f"""split_transfer prob: {p_split_trf}
-transfer prob: {round(p_trf, 3)}""")
-assert sum([p_split_trf, p_trf]) == 1
-
-all_scens3 = []
-i = 0
-while i < 3000:
-    drop_amt1.reset_cache()
-    drop_amt1.balance = dist_behav1.trf_min * 2
-    assert drop_amt1.balance < dist_behav1.atm_min, "Balance exceeds atm_min"
-    assert drop_amt1.balance < dist_behav1.trf_max, "Balance exceeds trf_max"
-    dist_behav1.sample_scenario()
-    all_scens3.append(dist_behav1.scen)
-    i += 1
-all_scens_ser3 = pd.Series(all_scens3)
-
-all_scens_ser3.value_counts(normalize=True)
-
-# `split_rate` = 0.7  
-# баланс меньше чем `DistBehaviorHandler.trf_min * 2`
-
-all_scens4 = []
-i = 0
-while i < 3000:
-    drop_amt1.reset_cache()
-    drop_amt1.balance = dist_behav1.trf_min + 1000
-    assert drop_amt1.balance < dist_behav1.trf_min * 2, "Balance exceeds the limit"
-    dist_behav1.sample_scenario()
-    all_scens4.append(dist_behav1.scen)
-    i += 1
-all_scens_ser4 = pd.Series(all_scens4)
-
-all_scens_ser4.value_counts(normalize=True)
-
-
-# **`in_chunks_val`**
-
-# Список сценариев
-# "split_money"
-# "one_purchase"
-
-dist_behav1.scen = "split_money"
-dist_behav1.in_chunks_val()
-dist_behav1.in_chunks
-
-# **`stop_after_decline`**
-
-dist_behav1.attempts = 2
-dist_behav1.stop_after_decline(declined=True)
-
-# **`reset_cache`**
-
-dist_behav1.attempts = 3
-dist_behav1.scen = "split_money"
-dist_behav1.online = True
-dist_behav1.in_chunks = False
-dist_behav1.reset_cache(all=True)
-dist_behav1.attempts, dist_behav1.scen, dist_behav1.online, dist_behav1.in_chunks
-
-# **`attempts_after_decline`**  
-# declined = True/False
-
-att_vals1 = []
-
-for _ in range(100):
-    dist_behav1.attempts_after_decline(declined=True)
-    att_vals1.append(dist_behav1.attempts)
-att_vals_ser1 = pd.Series(att_vals1)
-
-att_vals_ser1.agg(["min","mean", "max"])
-
-# **`deduct_attempts`**
-
-dist_behav1.attempts = 1
-dist_behav1.deduct_attempts(declined=True, receive=True)
-dist_behav1.attempts
-
-purch_behav1
+create_txn = CreateTxn(configs, base_classes) 
--- modules/data_generator/fraud/drops/builder.py
+++ modules/data_generator/fraud/drops/builder.py
@@ -1,3 +1,8 @@
+import pandas as pd
+import geopandas as gpd
+import numpy as np
+import pyarrow
+import os
 from dataclasses import dataclass
 from typing import Union
 
@@ -7,7 +12,7 @@ from data_generator.fraud.drops.behavior import DistBehaviorHandler, PurchBehavi
 from  data_generator.fraud.txndata import DropTxnPartData
 from data_generator.fraud.drops.time import DropTimeHandler
 
-# . Агрегатор базовых классов для дропов
+# 1. Агрегатор базовых классов для дропов
 
 @dataclass
 class DropBaseClasses:
@@ -24,3 +29,197 @@ class DropBaseClasses:
     part_data: DropTxnPartData
     time_hand: DropTimeHandler
     behav_hand: Union[DistBehaviorHandler, PurchBehaviorHandler]
+
+
+# 2. Конструктор объектов конфиг датаклассов
+class DropConfigBuilder:
+    """
+    Создание объектов датаклассов с конфигами.
+    ---------
+    Атрибуты:
+    ---------
+    base_cfg: dict. Конфиги из base.yaml
+    fraud_cfg: dict. Конфиги из fraud.yaml
+    drop_cfg: dict. Конфиги из drops.yaml
+    drops: gdp.GeoDataframe. Семплированные клиенты для дроп фрода.
+    """
+    def __init__(self, base_cfg: dict, fraud_cfg: dict, drop_cfg: dict):
+        """
+        base_cfg: dict. Конфиги из base.yaml
+        fraud_cfg: dict. Конфиги из fraud.yaml
+        drop_cfg: dict. Конфиги из drops.yaml
+        """
+        self.base_cfg = base_cfg
+        self.fraud_cfg = fraud_cfg
+        self.drop_cfg = drop_cfg
+        self.drops = None
+
+    def read_file(self, category, file_key):
+        """
+        category: str. Напр. 'cleaned_data', 'generated_data'.
+        file_key: str. Ключ к файлу в yaml конфиге.
+        """
+        path = self.base_cfg["data_paths"][category][file_key]
+        file_type = path.split(".")[-1]
+
+        if file_type == "csv":
+            return pd.read_csv(path)
+        
+        if file_type == "gpkg":
+            return gpd.read_file(path)
+        
+        if file_type == "parquet":
+            return pd.read_parquet(path, engine="pyarrow")
+
+
+    def estimate_drops_count(self, drop_type):
+        """
+        drop_type: str. Либо 'distributor' либо 'purchaser'
+        """
+        fraud_cfg = self.fraud_cfg
+        drop_cfg = self.drop_cfg
+
+        fraud_rate = fraud_cfg["fraud_rates"]["total"] # доля всего фрода от всех транзакций
+        drop_share = fraud_cfg["fraud_rates"]["drops"][drop_type] # Доля дропов указанного типа от всего фрода
+
+        out_lim = drop_cfg[drop_type]["out_lim"]
+
+        # отсюда посчитаем количество клиентов для дроп фрода с распределением денег
+        legit_txns = self.read_file(category="generated_data", file_key="legit_txns")
+        legit_count = legit_txns.shape[0]
+        # подсчет количества транзакций равных 1% от всех транзакций
+        # т.к. не все транзакции еще созданные, то считаем основываясь на количестве легальных транзакций и fraud rate
+        one_perc = round(legit_count / ((1 - fraud_rate) * 100))
+        # Один процент транзакций умножаем на долю транзакций дропов нужного типа 
+        # и делим на максимальное количество исх. транз-ций которое дроп может сделать до детекта.
+        # Так находим сколько примерно дропов будет под такой фрод
+        drops_count = round(one_perc * drop_share / out_lim) 
+        return drops_count
+    
+
+    def get_clients_for_drops(self, drop_type):
+        """
+        drop_type: str. Тип дропов: 'distributor' или 'purchaser'
+        """
+        drops_count = self.estimate_drops_count(drop_type=drop_type)
+        # legit_txns = self.read_file(category="generated_data", file_key="legit_txns")
+        clients_sample = self.read_file(category="cleaned_data", file_key="clients_sample")
+        all_clients = self.read_file(category="cleaned_data", file_key="clients_with_geo")
+
+        # Проверка наличия дропов другого типа. Чтобы не семплировать их случайно.
+        if drop_type == "distributor":
+            other_type_key = "purchase_drops"
+            file_key = "dist_drops"
+
+        elif drop_type == "purchaser":
+            other_type_key = "dist_drops"
+            file_key = "purchase_drops"
+
+        path = self.base_cfg["data_paths"]["generated_data"][other_type_key]
+
+        if os.path.exists(path):
+            other_drops = self.read_file(category="generated_data", file_key=other_type_key)
+        else:
+            other_drops = pd.DataFrame({"client_id": pd.Series(dtype="int64")}) # заглушка
+
+        # Фильруем клиентов не использованных для легальных транз., для compromised фрода
+        # и для дроп фрода другого типа  
+        not_used_clients =  all_clients.loc[~all_clients.client_id.isin(clients_sample.client_id) &
+                                            ~(all_clients.client_id.isin(other_drops.client_id))].copy()
+        
+        drops_samp = not_used_clients.sample(n=drops_count, replace=False).reset_index(drop=True)
+        write_to = self.base_cfg["data_paths"]["generated_data"][file_key]
+        drops_samp.to_file(write_to, layer="layer_name", driver="GPKG")
+        self.drops = drops_samp
+        return drops_samp
+
+
+    def build_dist_cfg(self):
+        """
+        Создать конфиг датакласс для дропов распределителей (distributors).
+        Возвращает объект DropDistributorCfg.
+        """
+        cleaned_data = "cleaned_data"
+        generated_data = "generated_data"
+        drop_cfg = self.drop_cfg
+        dist_cfg = drop_cfg["distributor"]
+        time_cfg = drop_cfg["time"]
+        
+        clients = self.get_clients_for_drops(drop_type="distributor")
+        timestamps = self.read_file(category=generated_data, file_key="timestamps")
+        accounts = self.read_file(category=generated_data, file_key="accounts")
+        outer_accounts = self.read_file(category=generated_data, file_key="outer_accounts").iloc[:,0] # нужны в виде серии
+        client_devices = self.read_file(category=cleaned_data, file_key="client_devices")
+        online_merchant_ids = self.read_file(category=cleaned_data, file_key="online_merchant_ids").iloc[:,0] # нужны в виде серии
+        cities = self.read_file(category=cleaned_data, file_key="districts_ru")
+        in_lim = dist_cfg["in_lim"]
+        out_lim = dist_cfg["out_lim"]
+        period_in_lim = dist_cfg["period_in_lim"]
+        period_out_lim = dist_cfg["period_out_lim"]
+        inbound_amt = drop_cfg["inbound_amt"]
+        split_rate = dist_cfg["split_rate"]
+        chunks = dist_cfg["chunks"]
+        trf_max = dist_cfg["trf_max"]
+        reduce_share = dist_cfg["reduce_share"]
+        round = dist_cfg["round"]
+        lag_interval = time_cfg["lag_interval"]
+        two_way_delta = time_cfg["two_way_delta"]
+        pos_delta = time_cfg["pos_delta"]
+        attempts = dist_cfg["attempts"]
+        to_drops = dist_cfg["to_drops"]
+        crypto_rate = dist_cfg["crypto_rate"]
+
+        return DropDistributorCfg(clients=clients, timestamps=timestamps, accounts=accounts, \
+                                  outer_accounts=outer_accounts, client_devices=client_devices, \
+                                  online_merchant_ids=online_merchant_ids, cities=cities, in_lim=in_lim, 
+                                  out_lim=out_lim, period_in_lim=period_in_lim, period_out_lim=period_out_lim, \
+                                  lag_interval=lag_interval, two_way_delta=two_way_delta, pos_delta=pos_delta, \
+                                  split_rate=split_rate, chunks=chunks, inbound_amt=inbound_amt, round=round, \
+                                  trf_max=trf_max, reduce_share=reduce_share, attempts=attempts, to_drops=to_drops, \
+                                  crypto_rate=crypto_rate
+                                  )
+
+
+    def build_purch_cfg(self):
+        """
+        Создать конфиг датакласс для дропов покупателей (purchasers).
+        Возвращает объект DropPurchaserCfg.
+        """
+        cleaned_data = "cleaned_data"
+        generated_data = "generated_data"
+        drop_cfg = self.drop_cfg
+        purch_cfg = drop_cfg["purchaser"]
+        time_cfg = drop_cfg["time"]
+        
+        clients = self.get_clients_for_drops(drop_type="purchaser")
+        timestamps = self.read_file(category=generated_data, file_key="timestamps")
+        accounts = self.read_file(category=generated_data, file_key="accounts")
+        client_devices = self.read_file(category=cleaned_data, file_key="client_devices")
+        online_merchant_ids = self.read_file(category=cleaned_data, \
+                                             file_key="online_merchant_ids").iloc[:,0] # нужны в виде серии
+        categories = self.read_file(category=cleaned_data, file_key="drop_purch_cats")
+        cities = self.read_file(category=cleaned_data, file_key="districts_ru")
+        in_lim = purch_cfg["in_lim"]
+        out_lim = purch_cfg["out_lim"]
+        period_in_lim = purch_cfg["period_in_lim"]
+        period_out_lim = purch_cfg["period_out_lim"]
+        inbound_amt = drop_cfg["inbound_amt"]
+        split_rate = purch_cfg["split_rate"]
+        chunks = purch_cfg["chunks"]
+        amt_max = purch_cfg["amt_max"]
+        reduce_share = purch_cfg["reduce_share"]
+        round = purch_cfg["round"]
+        lag_interval = time_cfg["lag_interval"]
+        two_way_delta = time_cfg["two_way_delta"]
+        pos_delta = time_cfg["pos_delta"]
+        attempts = purch_cfg["attempts"]
+
+        return DropPurchaserCfg(clients=clients, timestamps=timestamps, accounts=accounts, \
+                                client_devices=client_devices, \
+                                online_merchant_ids=online_merchant_ids, categories=categories, 
+                                cities=cities, in_lim=in_lim, out_lim=out_lim, period_in_lim=period_in_lim, \
+                                period_out_lim=period_out_lim, lag_interval=lag_interval, \
+                                two_way_delta=two_way_delta, pos_delta=pos_delta, split_rate=split_rate, \
+                                chunks=chunks, inbound_amt=inbound_amt, round=round, \
+                                amt_max=amt_max, reduce_share=reduce_share, attempts=attempts
+                                )
