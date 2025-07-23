3a37350d72f790702a635e7bb21a792959787e0f
iaroslav-dzh <mutuandrea94@gmail.com>
2025-07-23 11:41:55 +0300

    Reapply "split notebooks to clean and dev"
    This reverts commit 168fa993bb19a2fdfd8cf4efd5f7cab522b6d1d5.

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name="fraud_detection",
     version="0.1",
-    packages=find_packages(where="modules"),
-    package_dir={"": "modules"},
+    packages=find_packages(where="data_generator"),
+    package_dir={"": "data_generator"},
 )
